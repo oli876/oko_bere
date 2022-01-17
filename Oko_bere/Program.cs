325c38048f9886f2a2cc200b1288589279390fb2
oli876 <89512205+oli876@users.noreply.github.com>
2022-01-17 20:24:05 +0100

    Add files via upload

---
+++ Oko_bere/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Oko_bere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Začínáme hru oko bere.");
+			Random nahodne = new Random();
+			int karta = nahodne.Nex­t(0, 11);
+			int body = 0;
+		
+			Console.WriteLine("Máš " + body, "bodů");
+			Console.WriteLine("Chceš další kartu? ");
+			string odpoved = Console.ReadLine();
+
+			if (odpoved == "ano")
+			{
+				karta += nahodne.Nex­t(0, 11);
+
+				Console.Write("Otočil jsi kartu: " + karta);
+				body += karta;
+			}
+
+			else if (odpoved == "ne")
+			{
+				if (body <= 21)
+				{
+					Console.WriteLine("Trefa - jsi vítěz. Máš " + body, "bodů.");
+				}
+				else if (body > 21)
+				{
+					Console.WriteLine("Prohrál jsi! Máš " + body, "bodů a to je moc!");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Co to říkáš? Nerozumím ti - buď ano nebo ne");
+			}
+			Console.ReadKey();
+			
+		}
+			
+       
+    }
+}
